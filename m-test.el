89d86b6ce2fc70d5cf9ed811bad945a59c16a5ad
Lyn Levenick <346549+lynlevenick@users.noreply.github.com>
2021-03-23 10:59:11 -0700

    Rewrite to use a pass-based system

--- m-test.el
+++ m-test.el
@@ -6,20 +6,65 @@
 (require 'ert)
 (require 'm)
 
-(defvar m-test-latest-0-value 0)
-(m-defun m-test-latest-0 ()
-  (cl-incf m-test-latest-0-value))
+(defmacro m-test-valid (arity &rest keywords)
+  "Define a test for a function definition of ARITY with KEYWORDS.
 
-(ert-deftest m-latest-0 ()
-  (setf m-test-latest-0-value 0)
-  (should (= (m-test-latest-0) (m-test-latest-0))))
+Used to ensure valid runnable code is generated for a given set of keywords."
+  (let* ((base-name (concat "test-valid_"
+                            (cl-loop for keyword in (sort (cl-loop for keyword in keywords
+                                                                   if (keywordp keyword)
+                                                                   collect keyword)
+                                                          #'string<)
+                                     concat (substring (symbol-name keyword) 1)
+                                     concat "="
+                                     concat (symbol-name (plist-get keywords keyword))
+                                     concat "_")
+                            "arity="
+                            (number-to-string arity)))
+         (test-name (intern base-name))
+         (var-name (intern (concat base-name "_value")))
+         (fn-name (intern (concat base-name "_fn"))))
+    `(progn
+       (declare-function ,fn-name ,buffer-file-name)
+       (ert-deftest ,test-name ()
+         (defvar ,var-name 0)
+         (m-defun ,fn-name
+             ,(cl-loop for i from 1 to arity
+                       collect (intern (concat "arg" (number-to-string i))))
+           (memo ,@keywords)
+           (cl-incf ,var-name))
+
+         (should (= (,fn-name ,@(cl-loop for i from 1 to arity collect i))
+                    (,fn-name ,@(cl-loop for i from 1 to arity collect i))))))))
+
+(m-test-valid 0)
+(m-test-valid 1)
+(m-test-valid 2)
+(m-test-valid 0 :invalidate-on edit)
+(m-test-valid 1 :invalidate-on edit)
+(m-test-valid 2 :invalidate-on edit)
+(m-test-valid 0 :buffer-local t)
+(m-test-valid 1 :buffer-local t)
+(m-test-valid 2 :buffer-local t)
+(m-test-valid 0 :invalidate-on edit :buffer-local t)
+(m-test-valid 1 :invalidate-on edit :buffer-local t)
+(m-test-valid 2 :invalidate-on edit :buffer-local t)
+(m-test-valid 1 :storage hash)
+(m-test-valid 2 :storage hash)
+(m-test-valid 1 :storage hash :invalidate-on edit)
+(m-test-valid 2 :storage hash :invalidate-on edit)
+(m-test-valid 1 :storage hash :buffer-local t)
+(m-test-valid 2 :storage hash :buffer-local t)
+(m-test-valid 1 :storage hash :invalidate-on edit :buffer-local t)
+(m-test-valid 2 :storage hash :invalidate-on edit :buffer-local t)
+
+;; latest; 1 argument
 
 (defvar m-test-latest-1-value 0)
 (m-defun m-test-latest-1 (_)
   (cl-incf m-test-latest-1-value))
 
-(ert-deftest m-latest-1 ()
-  (setf m-test-latest-1-value 0)
+(ert-deftest test-latest-1 ()
   (let* ((result-0 (m-test-latest-1 'a))
          (result-1 (m-test-latest-1 'a))
          (result-2 (m-test-latest-1 'b))
@@ -29,12 +74,13 @@
     (should (= result-2 result-3))
     (should (/= result-3 (m-test-latest-1 'a)))))
 
+;; latest; 2+ arguments
+
 (defvar m-test-latest-many-value 0)
 (m-defun m-test-latest-many (_0 _1)
   (cl-incf m-test-latest-many-value))
 
-(ert-deftest m-latest-many ()
-  (setf m-test-latest-many-value 0)
+(ert-deftest test-latest-many ()
   (let* ((result-0 (m-test-latest-many 'a 'b))
          (result-1 (m-test-latest-many 'a 'b))
          (result-2 (m-test-latest-many 'c 'b))
@@ -46,12 +92,13 @@
     (should (/= result-3 result-4))
     (should (/= result-4 (m-test-latest-many 'c 'e)))))
 
+;; latest; 1 keyword argument
+
 (defvar m-test-latest-keyword-value 0)
 (m-defun m-test-latest-keyword (&optional _0)
   (cl-incf m-test-latest-keyword-value))
 
-(ert-deftest m-latest-keyword ()
-  (setf m-test-latest-keyword-value 0)
+(ert-deftest test-latest-keyword ()
   (let* ((result-0 (m-test-latest-keyword 'a))
          (result-1 (m-test-latest-keyword 'a))
          (result-2 (m-test-latest-keyword))
@@ -61,14 +108,15 @@
     (should (= result-2 result-3))
     (should (/= result-3 (m-test-latest-keyword 'a)))))
 
+;; latest; 0 arguments; buffer-local
+
 (defvar m-test-latest-buffer-local-value 0)
 (m-defun m-test-latest-buffer-local ()
-  :buffer-local t
+  (memo :buffer-local t)
 
   (cl-incf m-test-latest-buffer-local-value))
 
-(ert-deftest m-latest-buffer-local ()
-  (setf m-test-latest-buffer-local-value 0)
+(ert-deftest test-latest-buffer-local ()
   (let* ((result-0 nil)
          (result-1 nil)
          (result-2 nil)
@@ -82,20 +130,52 @@
             result-3 (m-test-latest-buffer-local)))
     (should (= result-0 result-1))
     (should (/= result-1 result-2))
-    (should (= result-2 result-3))
-    (should (/= result-3 (m-test-latest-buffer-local)))))
+    (should (= result-2 result-3))))
+
+;; latest; 0 arguments; on edit
+
+(defvar m-test-latest-invalidate-value 0)
+(m-defun m-test-latest-invalidate ()
+  (memo :invalidate-on edit)
+
+  (cl-incf m-test-latest-invalidate-value))
+
+(ert-deftest test-latest-invalidate ()
+  (let* ((result-0 nil)
+         (result-1 nil)
+         (result-2 nil)
+         (result-3 nil))
+    (with-temp-buffer
+      (setf result-0 (m-test-latest-invalidate)
+            result-1 (m-test-latest-invalidate))
+      (insert "characters")
+      (setf result-2 (m-test-latest-invalidate)
+            result-3 (m-test-latest-invalidate)))
+    (should (= result-0 result-1))
+    (should (/= result-1 result-2))
+    (should (= result-2 result-3))))
+
+;; hash; 0 arguments
+
+(ert-deftest test-hash-0 ()
+  (should-error
+   (m-defun m-test-hash-0 ()
+     (memo :storage hash)
+
+     0)))
+
+;; hash; 1 argument
 
 (defvar m-test-hash-1-value 0)
 (m-defun m-test-hash-1 (_)
-  :storage hash
+  (memo :storage hash)
 
   (cl-incf m-test-hash-1-value))
 
 ;; Hash tests assume nothing will cause garbage collection of the
-;; keys or values during execution
+;; keys or values during execution because hash storage is weak
 ;; TODO: let-bind GC variables to enforce that?
-(ert-deftest m-hash-1 ()
-  (setf m-test-hash-1-value 0)
+(ert-deftest test-hash-1 ()
   (let* ((result-0 (m-test-hash-1 'a))
          (result-1 (m-test-hash-1 'a))
          (result-2 (m-test-hash-1 'b))
@@ -107,13 +187,15 @@
     (should (/= result-3 result-4))
     (should (= result-4 result-0))))
 
+;; hash; 2+ arguments
+
 (defvar m-test-hash-many-value 0)
 (m-defun m-test-hash-many (_0 _1)
-  :storage hash
+  (memo :storage hash)
 
   (cl-incf m-test-hash-many-value))
 
-(ert-deftest m-hash-many ()
+(ert-deftest test-hash-many ()
   (setf m-test-hash-many-value 0)
   (let* ((result-0 (m-test-hash-many 'a 'b))
          (result-1 (m-test-hash-many 'a 'b))
@@ -129,3 +211,14 @@
     (should (/= result-4 result-5))
     (should (= result-5 result-3))
     (should (= result-6 result-0))))
+
+;; ensure documentation is preserved
+
+(m-defun m-test-docs ()
+  "Return zero."
+  (memo :storage latest)
+
+  0)
+
+(ert-deftest test-docs ()
+  (should (string-equal "Return zero." (documentation #'m-test-docs))))
--- m.el
+++ m.el
@@ -2,8 +2,8 @@
 
 ;; Author: Lyn Levenick
 ;; Package-Requires: ((emacs "26.3"))
-;; Package-Version: 1.1.0
-;; URL: https://github.com/lynlevenick/emacs-m
+;; Package-Version: 2.0.0
+;; URL: https://github.com/lynlevenick/emacs-memo
 
 ;; This file is not part of GNU Emacs.
 
@@ -23,6 +23,34 @@
 (eval-when-compile
   (require 'cl-lib))
 
+(defconst m--sentinel (make-symbol "m--sentinel")
+  "Sentinel value for ‘m-defun’ signaling no value present.
+
+Not interned; the value bound to ‘m--sentinel’ is tested at
+call time.")
+
+(defvar m--variables nil
+  "Variables to be bound for current expansion in ‘m-defun’.
+
+Binding takes place around the entire declaration, used to
+share definitions between expansions.")
+
+(defun m--form-or-progn (forms)
+  "Return a length 1 form of FORMS."
+
+  (if (> (length forms) 1)
+      `(progn ,@forms)
+    (car forms)))
+
+(defun m--register-variables (&rest decls)
+  "Register DECLS with ‘m--variables’."
+
+  (cl-loop for decl in decls
+           if (alist-get (car decl) m--variables)
+           do (user-error "Symbol %s already bound" (car decl))
+           else
+           do (push decl m--variables)))
+
 (defmacro m--with-symbols (key names &rest body)
   "Bind NAMES to interned symbols then eval BODY.
 
@@ -30,9 +58,10 @@ Each element within NAMES is a SYMBOL.
 
 Each SYMBOL specifies that the variable named by SYMBOL should
 be bound to a symbol constructed using ‘intern’ and keyed with
-the value of KEY and each SYMBOL for uniqueness. Since symbols
-are interned they are accessible normally."
-  (declare (indent 1))
+the value of KEY and each SYMBOL for uniqueness. Byte-compiles
+are consistent when using interned symbols, but they allow any
+code to access them."
+  (declare (indent 2))
 
   `(let ,(cl-loop for name in names
                   collect `(,name (intern (concat "m--"
@@ -40,137 +69,244 @@ are interned they are accessible normally."
                                                   ,(symbol-name name)))))
      ,@body))
 
-(defconst m--sentinel (make-symbol "m--sentinel")
-  "Sentinel value for ‘m-defun’ signaling an uncalled method.")
+(defmacro memo (&rest _SPECS)
+  "Do not evaluate any arguments, and return nil.
 
-(defun m--latest (func arglist body props)
-  "Transform BODY to memoize the previous invocation.
+If a ‘memo’ form appears before a ‘m-defun’ body, SPECS
+specifies memoization configuration for the function,
+modifying the function declaration."
 
-Return a list (GLOBAL BODY) which specifies global-scoped
-declarations and the transformed body.
+  nil)
 
-FUNC is the name of the function. ARGLIST is the arguments that the
-function will receive. PROPS has the same meaning as in ‘m-defun’."
+;; list of functions that define variables for memoization??
+;;   could be skipped if a final pass does definitions, but
+;;   how would it know about them?
+;; list of functions that transform function body
+;; list of functions that wrap function body
+;; latter two insert un-evaluated placeholders (somehow??)
+;; which are replaced in a final pass, erroring for undefined
 
-  ;; TODO: let over defun may work better here when not buffer-local
-  ;;       though that would only work if ‘lexical-binding’ is set
-  ;; TODO: Abstract over N arguments instead of special casing 0-1-many?
-  ;; TODO: Can any architecture from here be generalized?
+;;; :storage
 
-  (m--with-symbols func (prev-args prev-value after-change current-args)
-    (let ((def-fn (if (plist-get props :buffer-local) #'defvar-local #'defvar))
-          (arity (length arglist)))
-      `((,@(when (> arity 0)
-             `((,def-fn ,prev-args m--sentinel)))
-         (,def-fn ,prev-value ,(if (> arity 0) 'nil 'm--sentinel))
-         ,@(pcase (plist-get props :clear-on)
-            ('nil nil)
-            ('edit
-             `((defun ,after-change (&rest _)
-                 (setf ,(if (> arity 0) prev-args prev-value) m--sentinel))
-               (add-hook 'after-change-functions #',after-change)))
-            (clear-on (user-error "Unknown clear-on: %s" clear-on))))
-        (,(pcase arity
-           (0 `(if (eq ,prev-value m--sentinel)
+;; latest
+
+(defun m--storage-latest (name arglist _props body)
+  "See ‘m--transform-body-list’.
+
+NAME, ARGLIST, PROPS, and BODY have the same meaning as in
+‘m--transform-body-list’."
+
+  (m--with-symbols name (prev-args prev-value current-args)
+    (pcase (length arglist)
+      (0 (m--register-variables `(,prev-value m--sentinel))
+         `((if (eq ,prev-value m--sentinel)
+               (setf ,prev-value ,body)
+             ,prev-value)))
+      (1 (m--register-variables `(,prev-args m--sentinel)
+                                   `(,prev-value nil))
+         `((if (equal ,prev-args ,@arglist)
+               ,prev-value
+             (prog1 ; body may fail, doesn't set prev-args if so
+                 (setf ,prev-value ,body)
+               (setf ,prev-args ,@arglist)))))
+      (_ (m--register-variables `(,prev-args m--sentinel)
+                                   `(,prev-value nil))
+         `((let ((,current-args (list ,@arglist)))
+             (if (equal ,prev-args ,current-args)
+                 ,prev-value
+               (prog1 ; body may fail, doesn't set prev-args if so
                    (setf ,prev-value ,body)
-                 ,prev-value))
-           (1 `(if (equal ,prev-args ,@arglist)
-                   ,prev-value
-                 (prog1
-                     (setf ,prev-value ,body)
-                   (setf ,prev-args ,@arglist))))
-           (_ `(let ((,current-args (list ,@arglist)))
-                   (if (equal ,prev-args ,current-args)
-                       ,prev-value
-                     (prog1
-                         (setf ,prev-value ,body)
-                       (setf ,prev-args ,current-args)))))))))))
+                 (setf ,prev-args ,current-args)))))))))
 
-(defun m--hash (func arglist body props)
-  "Transform BODY to memoize previous invocations into a hash.
+(defun m--storage-latest-invalidate (name arglist _props)
+  "See ‘m--invalidate-method-list’.
 
-Return a list (GLOBAL BODY) which specifies global-scoped
-declarations and the transformed body.
+NAME, ARGLIST, and PROPS have the same meaning as in
+‘m--invalidate-method-list’."
 
-FUNC is the name of the function. ARGLIST is the arguments that the
-function will receive. PROPS has the same meaning as in ‘m-defun’."
+  (m--with-symbols name (prev-args prev-value)
+    `(setf ,(if (> (length arglist) 0) prev-args prev-value)
+           m--sentinel)))
 
-  ;; TODO: See ‘m--latest’ TODOs
-  ;; Not a huge fan of the massive amount of repetition here :'(
+;; hash
 
-  (m--with-symbols func (prev-calls after-change cached current-args)
-    (let ((def-fn (if (plist-get props :buffer-local) #'defvar-local #'defvar))
-          (arity (length arglist)))
-      (when (= arity 0)
-        (user-error "Memoization into hash by zero arguments"))
-      `(((,def-fn ,prev-calls (make-hash-table :test 'equal :weakness t))
-         ,@(pcase (plist-get props :clear-on)
-             ('nil nil)
-             ('edit
-              `((defun ,after-change (&rest _)
-                  (clrhash ,prev-calls))
-                (add-hook 'after-change-functions #',after-change)))
-             (clear-on (user-error "Unknown clear-on: %s" clear-on))))
-        (,(pcase arity
-            (1 `(let ((,cached (gethash ,@arglist ,prev-calls m--sentinel)))
-                  (if (eq ,cached m--sentinel)
-                      (puthash ,@arglist ,body ,prev-calls)
-                    ,cached)))
-            (_ `(let* ((,current-args (list ,@arglist))
-                       (,cached (gethash ,current-args ,prev-calls m--sentinel)))
-                  (if (eq ,cached m--sentinel)
-                      (puthash ,current-args ,body ,prev-calls)
-                    ,cached)))))))))
+(defun m--storage-hash (name arglist _props body)
+  "See ‘m--transform-body-list’.
+
+NAME, ARGLIST, PROPS, and BODY have the same meaning as in
+‘m--transform-body-list’."
+
+  (m--with-symbols name (prev-calls cached current-args)
+    (m--register-variables
+     `(,prev-calls (make-hash-table :test 'equal :weakness t)))
+    (pcase (length arglist)
+      (0 (user-error "Memoization into hash by zero arguments"))
+      (1 `((let ((,cached (gethash ,@arglist ,prev-calls m--sentinel)))
+             (if (eq ,cached m--sentinel)
+                 (puthash ,@arglist ,body ,prev-calls)
+               ,cached))))
+      (_ `((let* ((,current-args (list ,@arglist))
+                  (,cached (gethash ,current-args ,prev-calls
+                                    m--sentinel)))
+             (if (eq ,cached m--sentinel)
+                 (puthash ,current-args ,body ,prev-calls)
+               ,cached)))))))
+
+(defun m--storage-hash-invalidate (name _arglist _props)
+  "See ‘m--invalidate-method-list’.
+
+NAME, ARGLIST, and PROPS have the same meaning as in
+‘m--invalidate-method-list’."
+
+  (m--with-symbols name (prev-calls)
+    `(clrhash ,prev-calls)))
+
+;;; :invalidate-on
+
+(defvar m--invalidate-method-list
+  `((latest . ,#'m--storage-latest-invalidate)
+    (hash . ,#'m--storage-hash-invalidate))
+  "List associating storage type to function invalidating that storage.
+
+Each element takes the form (STORAGE . FN) where STORAGE is
+the type of storage and FN is its macro expansion.
+
+FN is called as (FN NAME ARGLIST PROPS) and returns a form
+invalidating storage for the function NAME.")
+
+;; edit
+
+(defun m--invalidate-on-edit (name arglist props decl)
+  "See ‘m--transform-decl-list’.
+
+NAME, ARGLIST, PROPS, and DECL have the same meaning as in
+‘m--transform-decl-list’."
+
+  (m--with-symbols name (after-change)
+    `((defun ,after-change (&rest _)
+        ,(let* ((storage (or (plist-get props :storage) 'latest))
+                (invalidation (alist-get storage m--invalidate-method-list)))
+           (if invalidation
+               (funcall invalidation name arglist props)
+             (user-error "Unknown storage: %s" storage))))
+      (add-hook 'after-change-functions #',after-change)
+      ,@decl)))
+
+;;; meta pass - define variables
+
+(defun m--define-variables (_name _arglist props decl)
+  "See ‘m--transform-decl-list’.
+
+NAME, ARGLIST, PROPS, and DECL have the same meaning as in
+‘m--transform-decl-list’."
+
+  (prog1
+      (let ((buffer-local (plist-get props :buffer-local)))
+        (if (and lexical-binding (not buffer-local))
+            ;; lexical binding and global, can let-over-defun
+            `((let ,m--variables
+                ,@decl))
+          ;; buffer local or dynamic binding, can't let-over-defun
+          (let ((defvar-fn (if buffer-local #'defvar-local #'defvar)))
+            `(,@(cl-loop for decl in m--variables
+                         collect `(,defvar-fn ,@decl))
+              ,@decl))))
+    (setf m--variables nil)))
+
+;;; body and decl transformations
+
+(defvar m--transform-body-list
+  `((:storage
+     (:default . ,#'m--storage-latest)
+     (latest . ,#'m--storage-latest)
+     (hash . ,#'m--storage-hash)))
+  "List of memoization properties and their expansion.
+
+Each element takes the form (PROP HANDLERS...) where HANDLERS
+is a list of the form (VALUE . FN) where VALUE is the value of
+PROP in PROPS and FN is its macro expansion.
+Keyword :default may be used instead of VALUE to specify
+transformation to take place if no PROP matches or is specified
+in PROPS.
+
+Evaluated in order against PROPS.
+
+FN is called as (FN NAME ARGLIST PROPS BODY) and returns a new
+function body. NAME, ARGLIST, PROPS, and BODY have the same
+meaning as in ‘m-defun’.")
+
+(defvar m--transform-decl-list
+  `((:invalidate-on
+     (edit . ,#'m--invalidate-on-edit))
+    (:m--define-variables
+     (:default . ,#'m--define-variables)))
+  "See ‘m--transform-body-list’.
+
+FN is instead called as (FN NAME ARGLIST PROPS DECL) and
+returns a new declaration.")
+
+;; would like to have:
+;; :key-on  Additional key to memoization.
+;;          May be nil or an expression to evaluate. Default is nil.
+;;          FIXME: implement this - it would have to essentially
+;;          extend the arglist for all _inner_ passes?? eval
+;;          outside to inside? lmao
 
 ;;;###autoload
 (defmacro m-defun (name arglist &rest body)
   "Define NAME as a memoized function.
 
-NAME, ARGLIST, DOCSTRING, DECL, and BODY have the same meaning as in ‘defun’.
-Optional PROPS are a group of configuration options for the memoization.
+NAME, ARGLIST, DOCSTRING, DECL, and BODY have the same meaning
+as in ‘defun’.
 
-:buffer-local  Whether storage is buffer-local.
-               May be nil or t. Default is nil.
-:clear-on      When storage is invalidated.
-               May be nil or the symbol ‘edit’. Default is nil.
-:storage       Storage to be used during memoization.
-               May be one of the symbols ‘latest’ or ‘hash’.
-               Default is ‘latest’.
+MEMO is an declaration, optional, of the form (memo PROPS...)
+where PROPS is a property list, interpreted as follows:
 
-\(fn NAME ARGLIST &optional DOCSTRING DECL PROPS... &rest BODY)"
+:buffer-local   Whether cache is buffer-local.
+                May be nil or t. Default is nil.
+:invalidate-on  When cache is invalidated.
+                May be nil or the symbol ‘edit’. Default is nil.
+:storage        Backing storage for the memoization.
+                May be one of the symbols ‘latest’ or ‘hash’.
+                Default is ‘latest’.
+
+\(fn NAME ARGLIST &optional DOCSTRING DECL MEMO &rest BODY)"
   (declare (debug defun) (doc-string 3) (indent 2))
 
-  (let ((body-prefix nil)
-        (props nil))
+  (let ((narrow-arglist ; excludes keywords
+         (cl-loop for arg in arglist
+                  unless (or (eq arg '&optional)
+                             (eq arg '&rest))
+                  collect arg))
+        (body-prefix) (props) (decl))
+
     ;; Take docstring and decl
     (when (eq (type-of (car body)) 'string)
       (push (pop body) body-prefix))
     (when (eq (car-safe (car body)) 'declare)
       (push (pop body) body-prefix))
-    (cl-callf reverse body-prefix)
+    (cl-callf nreverse body-prefix)
+
     ;; Take props
-    (while (keywordp (car body))
-      (push (pop body) props)
-      (push (pop body) props))
-    (cl-callf reverse props)
-    ;; Construct function
-    (pcase-let* ((storage (or (plist-get props :storage) 'latest))
-                 (memo-fn (intern (concat "m--" (symbol-name storage))))
-                 (`(,global ,transformed-body)
-                  (funcall memo-fn name
-                           (cl-loop for arg in arglist ; don't pass keywords
-                                    unless (or (eq arg '&optional)
-                                               (eq arg '&rest))
-                                    collect arg)
-                           (if (> (length body) 1)
-                               `(progn ,@body)
-                             (car body))
-                           props)))
-      `(progn
-         ,@global
-         (defun ,name ,arglist
-           ,@body-prefix
-           ,@transformed-body)))))
+    (when (eq (car-safe (car body)) 'memo)
+      (setf props (cdr (pop body))))
+
+    ;; Construct body
+    (cl-callf m--form-or-progn body)
+    (cl-loop for (prop . handlers) in m--transform-body-list
+             do (when-let ((prop-val (or (plist-get props prop) :default))
+                           (handler (alist-get prop-val handlers)))
+                  (setf body (funcall handler name narrow-arglist props body))))
+
+    ;; Construct decl
+    (setf decl `((defun ,name ,arglist ,@body-prefix ,@body)))
+    (cl-loop for (prop . handlers) in m--transform-decl-list
+             do (when-let ((prop-val (or (plist-get props prop) :default))
+                           (handler (alist-get prop-val handlers)))
+                  (setf decl (funcall handler name narrow-arglist props decl))))
+
+    ;; Return a single form
+    (m--form-or-progn decl)))
 
 (provide 'm)
 ;;; m.el ends here
